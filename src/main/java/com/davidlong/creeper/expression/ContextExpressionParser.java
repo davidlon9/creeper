e387341d17332534a7761ef027b6f9c86f02a804
davidlon9 <744941378@qq.com>
2020-08-23 00:56:06 +0800

    表达式中若包含ContextStore中的参数，且参数值为空或未设，则警告

--- src/main/java/com/davidlong/creeper/expression/ContextExpressionParser.java
+++ src/main/java/com/davidlong/creeper/expression/ContextExpressionParser.java
@@ -1,11 +1,18 @@
 package com.davidlong.creeper.expression;
 
+import com.davidlong.creeper.resolver.util.WrapUtil;
+import org.apache.log4j.Logger;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ParserContext;
+import org.springframework.expression.TypedValue;
+import org.springframework.expression.common.CompositeStringExpression;
+import org.springframework.expression.common.ExpressionUtils;
+import org.springframework.expression.spel.standard.SpelExpression;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 public class ContextExpressionParser {
+    private static Logger logger=Logger.getLogger(ContextExpressionParser.class);
     private SpelExpressionParser parser;
     private EvaluationContext evaluationContext;
 
@@ -31,10 +38,43 @@ public class ContextExpressionParser {
     }
 
     public Object parse(String str){
-        return parseExpression(str).getValue(evaluationContext);
+        Expression expression = parseExpression(str);
+        if(expression instanceof CompositeStringExpression){
+            String value = getCompositeStringValue((CompositeStringExpression) expression);
+            return ExpressionUtils.convertTypedValue(evaluationContext, new TypedValue(value), String.class);
+        }
+        return expression.getValue(evaluationContext);
     }
 
     public <T> T parse(String str,Class<T> expectedResultType){
-        return parseExpression(str).getValue(evaluationContext,expectedResultType);
+        Expression expression = parseExpression(str);
+        if(expression instanceof CompositeStringExpression){
+            String value = getCompositeStringValue((CompositeStringExpression) expression);
+            return ExpressionUtils.convertTypedValue(evaluationContext, new TypedValue(value), expectedResultType);
+        }
+        return expression.getValue(evaluationContext,expectedResultType);
+    }
+
+    private String getCompositeStringValue(CompositeStringExpression expression) {
+        CompositeStringExpression cse= expression;
+        StringBuilder sb = new StringBuilder();
+        Expression[] expressions = cse.getExpressions();
+        for (Expression e : expressions) {
+            if (e instanceof SpelExpression) {
+                SpelExpression spelExpression = (SpelExpression) e;
+                String value = spelExpression.getValue(evaluationContext, String.class);
+                if("".equals(value) || value == null){
+                    logger.warn("expression "+ WrapUtil.enDoubleQuote(spelExpression.getExpressionString())+" parsed value is null or empty, please make sure ContextParamStore contains params which names is in expression string.");
+                }else{
+                    sb.append(value);
+                }
+            }else{
+                String value = e.getValue(evaluationContext, String.class);
+                if (value != null) {
+                    sb.append(value);
+                }
+            }
+        }
+        return sb.toString();
     }
 }
