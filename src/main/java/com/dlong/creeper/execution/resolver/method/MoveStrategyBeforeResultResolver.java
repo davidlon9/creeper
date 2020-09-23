a24224f25afedf06606d1a22b4bcce24c4004d5e
davidlon9 <744941378@qq.com>
2020-09-23 20:15:26 +0800

    use ContinueActionResolver resolve ContinueAction now

--- src/main/java/com/dlong/creeper/execution/resolver/method/MoveStrategyBeforeResultResolver.java
+++ src/main/java/com/dlong/creeper/execution/resolver/method/MoveStrategyBeforeResultResolver.java
@@ -5,21 +5,15 @@ import com.dlong.creeper.exception.ExecutionException;
 import com.dlong.creeper.exception.UnsupportedReturnTypeException;
 import com.dlong.creeper.execution.context.ChainContext;
 import com.dlong.creeper.model.result.ExecutionResult;
-import com.dlong.creeper.model.seq.SequentialEntity;
 import org.apache.log4j.Logger;
 
 public class MoveStrategyBeforeResultResolver implements HandlerMethodResultResolver {
-    private static Logger logger=Logger.getLogger(MoveStrategyBeforeResultResolver.class);
+    private static Logger logger= Logger.getLogger(MoveStrategyBeforeResultResolver.class);
 
     @Override
     public ExecutionResult resolveResult(ExecutionResult executionResult, ChainContext context, Object methodResult) throws ExecutionException {
-        SequentialEntity next;
         if(methodResult instanceof ContinueAction){
-            next = executionResult.getOrginalSeq();
-            executionResult.setSkipExecute(true);
-            executionResult.setNextSeq(next);
-            logger.warn("before handler return ContinueAction to skip current loop in loop execution");
-            return executionResult;
+            return new ContinueActionResolver().resolveResult(executionResult,context,methodResult);
         }else{
             throw new UnsupportedReturnTypeException("BeforeHandler only support ContinueAction to skip current loop in loop execution, to skip normal execution please return Boolean value");
         }
