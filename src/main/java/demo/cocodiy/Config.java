0aec6c40cccbfddca892b87fbf0e80029032d054
davidlon9 <744941378@qq.com>
2020-10-11 10:14:23 +0800

    remove cache and commit

--- src/main/java/demo/cocodiy/Config.java
+++
@@ -1,5 +0,0 @@
-package demo.cocodiy;
-
-public class Config {
-    public static final int DEFAULT_INTERVAL=3000;
-}
--- src/main/java/demo/traiker/test/TestOrder.java
+++
@@ -1,6 +0,0 @@
-package demo.traiker.test;
-
-public class TestOrder {
-    public static void main(String[] args) {
-    }
-}
